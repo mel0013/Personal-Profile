574bfd5f7b3a705d564ea1532418ff6c50e179b6
Melinda Batang <meeelinda.b@gmail.com>
2022-02-05 22:50:53 +1100

    added code to close navbar after clicking on nav list

--- main.js
+++ main.js
@@ -17,3 +17,16 @@ scrollButton.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Select navigation links and close navbar
+// Set the checked attribute on the type=checkbox
+const links = document.querySelectorAll('.links a')
+const checkbox = document.querySelector('#nav-toggle')
+
+for ( const link of links ) {
+  link.onclick = handleClick
+}
+
+function handleClick() {
+  checkbox.checked = false
+}
